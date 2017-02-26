ce08a85a1a90de9fdcb36c26adbd8f7cf05a53e4
jochechavez <joche.echavezm@gmail.com>
2017-02-26 18:17:09 -0500

    add webhook

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+    req.query['hub.verify_token'] === 'secret-secret') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);          
+  }  
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
