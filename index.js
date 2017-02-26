d00ed13991f6e1fcf42a548ad2ea57c471ad3a99
jochechavez <joche.echavezm@gmail.com>
2017-02-26 18:41:27 -0500

    test

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ app.get('/webhook', function(req, res) {
 app.post('/webhook', function (req, res) {
   var data = req.body;
 
+  console.log(data);
+
   // Make sure this is a page subscription
   if (data.object === 'page') {
 
