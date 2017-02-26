c97752b6d93c99bc416f8cefbfad051b8525b738
jochechavez <joche.echavezm@gmail.com>
2017-02-26 18:15:07 -0500

    configure deploy2

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(process.env.port || 3000, function() { console.log('Serving port '+(process.env.port || 3000)) })
