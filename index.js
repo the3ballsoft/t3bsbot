dac1ff27c0cf4897f5f458937566c0456486ab58
jochechavez <joche.echavezm@gmail.com>
2017-02-26 19:14:46 -0500

    request fucking fucking requirement

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var bodyParser = require('body-parser')
-var rquest = require('request')
+var request = require('request')
 var app = express()
 
 app.use(bodyParser.json())
